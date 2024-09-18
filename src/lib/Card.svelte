56ba2687ccc72e1b08ca3c071be85c3d4c921611
Tobias <mail@henahax.de>
2024-09-18 17:35:15 +0200

    Update Card.svelte

--- src/lib/Card.svelte
+++ src/lib/Card.svelte
@@ -1,11 +1,27 @@
-<div class="Card flex w-96 flex-col rounded-2xl bg-white p-2">
-	<div class="flex h-full w-full flex-col rounded-xl bg-blue-700 p-4 pt-2">
-		<slot />
+<script lang="ts">
+	interface Props {
+		borderColor: string;
+		cardColor: string;	
+		textColor: string;	
+	}
+
+	let { cardColor, borderColor, textColor }: Props = $props();
+</script>
+
+<div id="Border" class="m-8 flex w-96 flex-col rounded-2xl p-3" style="--borderColor: {borderColor}; --textColor: {textColor}">
+	<div id="Card" class="flex h-full w-full flex-col rounded-xl p-4 pt-2" style="--cardColor: {cardColor}">
+		<slot/>
 	</div>
 </div>
 
 <style>
-	.Card {
-		aspect-ratio: 63 / 88;
+	#Border {
+		aspect-ratio: 3 / 5;
+		background-color: var(--borderColor);
+	}
+
+	#Card{
+		background-color: var(--cardColor);
+		color: var(--textColor);
 	}
 </style>
